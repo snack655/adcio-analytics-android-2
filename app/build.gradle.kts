30edca37bade3035db864de8864ae0ec98b63b5a
최민재 <choeminjae@choeminjaeui-MacBookPro-2.local>
2023-07-26 18:24:30 +0900

    feat: add basic libraries in buildSrc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,69 +1,63 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id(Plugins.application)
+    id(Plugins.kotlinAndroid)
+    id(Plugins.kapt)
 }
 
 android {
-    namespace = "ai.minjae.adcio_analytics"
-    compileSdk = 33
+    namespace = AppConfig.appNamespace
+    compileSdk = AppConfig.compileSdk
 
     defaultConfig {
-        applicationId = "ai.minjae.adcio_analytics"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        applicationId = AppConfig.applicationId
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = AppConfig.androidTestInstrumentation
         vectorDrawables {
-            useSupportLibrary = true
+            useSupportLibrary = AppConfig.useSupportLibrary
         }
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = AppConfig.minifyEnabled
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile(AppConfig.defaultProguardFile),
+                AppConfig.proguardConsumerRules
             )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = AppConfig.javaVersion
+        targetCompatibility = AppConfig.javaVersion
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = AppConfig.jvmTarget
     }
     buildFeatures {
-        compose = true
+        compose = AppConfig.compose
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = AppConfig.kotlinCompilerExtensionVersion
     }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += AppConfig.packagingOption
         }
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(Libraries.androidLibraries)
+    implementation(Libraries.composeLibraries)
+    implementation(platform(Libraries.composeBom))
+
+    testImplementation(Libraries.junit)
+    androidTestImplementation(Libraries.testLibraries)
+    androidTestImplementation(platform(Libraries.composeBom))
+    debugImplementation(Libraries.debugLibraries)
 }
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,18 @@
+object Versions {
+    // android
+    const val coreKtx = "1.10.1"
+    const val lifecycleRunTimeKtx = "2.6.1"
+
+    // compose
+    const val compose = "1.7.2"
+    const val composeBom = "2023.03.00"
+
+    // test
+    const val junit = "4.13.2"
+    const val extJunit = "1.1.5"
+    const val espressoCore = "3.5.1"
+
+    // plugins
+    const val android = "8.1.0"
+    const val kotlinAndroid = "1.8.10"
+}
