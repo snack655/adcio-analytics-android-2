c933c8c3fa4694aedeb00d81ce8dce726c332ff7
최민재 <cmjcmj1230@gmail.com>
2023-07-27 14:35:04 +0900

    feat: add analyticsException for handle event

---
+++ analytics/src/main/java/ai/minjae/analytics/exception/AnalyticsException.kt
@@ -0,0 +1,20 @@
+package ai.minjae.analytics.exception
+
+private const val NOT_INITIALIZED_EXCEPTION_MESSAGE = "[ADCIO Analytics] 초기화되지 않았습니다."
+private const val FILE_NOT_FOUND_EXCEPTION_MESSAGE = "[ADCIO Analytics] 파일을 찾을 수 없습니다."
+private const val EMPTY_ENV_EXCEPTION_MESSAGE = "[ADCIO Analytics] 환경 변수가 입력되지 않았습니다."
+
+class NotInitializedException : RuntimeException() {
+    override val message: String
+        get() = NOT_INITIALIZED_EXCEPTION_MESSAGE
+}
+
+class FileNotFoundException : RuntimeException() {
+    override val message: String
+        get() = FILE_NOT_FOUND_EXCEPTION_MESSAGE
+}
+
+class EmptyEnvFileException : RuntimeException() {
+    override val message: String
+        get() = EMPTY_ENV_EXCEPTION_MESSAGE
+}
